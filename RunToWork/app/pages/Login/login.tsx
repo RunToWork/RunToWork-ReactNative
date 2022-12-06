18e375da52a46a1ec2c2c6fb6b97dd111a0725ec
greeneeya <suzythetravelerr@gmail.com>
2022-12-06 23:34:58 +0900

    navigation 적용

---
+++ RunToWork/app/pages/Login/login.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export const Login = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
