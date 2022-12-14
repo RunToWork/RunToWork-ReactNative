d16570b8dca4f9282b9d219574aedf2d4b1b4d34
greeneeya <suzythetravelerr@gmail.com>
2022-12-15 01:12:27 +0900

    회원가입 페이지 UI 완료

--- RunToWork/app/screens/SignUp/signUp.tsx
+++ RunToWork/app/screens/SignUp/signUp.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 import {Text, TextInput, View} from 'react-native';
-import {Container, LoginInput} from '../Login/styled';
-import {InputLable} from './styled';
+import {
+  Container,
+  LoginButton,
+  LoginButtonText,
+  LoginInput,
+  LoginInputError,
+} from '../Login/styled';
+import {
+  InputGroup,
+  InputLable,
+  MandatoryInput,
+  SignUpInfo,
+  SignUpInput,
+} from './styled';
 
 export const SignUp = () => {
   return (
     <Container>
-      <InputLable>아이디</InputLable>
-      <LoginInput placeholder="영문, 숫자 5-11자" />
-
-      <InputLable>비밀번호</InputLable>
-      <LoginInput placeholder="숫자, 영문, 특수조합 최소 8자" />
-      <LoginInput placeholder="비밀번호 재입력" />
-      <InputLable>이메일</InputLable>
-      <LoginInput />
+      <InputLable>
+        아이디<MandatoryInput>*</MandatoryInput>
+      </InputLable>
+      <SignUpInput placeholder="영문, 숫자 5-11자" />
+      <InputLable>
+        비밀번호<MandatoryInput>*</MandatoryInput>
+      </InputLable>
+      <SignUpInput placeholder="숫자, 영문, 특수조합 최소 8자" />
+      <SignUpInput placeholder="비밀번호 재입력" />
+      <InputLable>
+        이메일<MandatoryInput>*</MandatoryInput>
+      </InputLable>
+      <SignUpInput />
+      <SignUpInfo>계정 분실 시 본인인증 정보로 활용됩니다.</SignUpInfo>
+      <LoginButton>
+        <LoginButtonText>가입하기</LoginButtonText>
+      </LoginButton>
     </Container>
   );
 };
--- RunToWork/app/screens/SignUp/styled.ts
+++ RunToWork/app/screens/SignUp/styled.ts
@@ -1,5 +1,36 @@
 import styled from '@emotion/native';
 
+export const SignUpInput = styled.TextInput`
+  border-color: lightgray;
+  border-width: 1px;
+  margin-top: 10px;
+  width: 90%;
+  font-family: 'Pretendard-Medium';
+  border-radius: 4px;
+`;
+
+export const MandatoryInput = styled.Text`
+  color: red;
+  font-family: 'Pretendard-Bold';
+  font-size: 10px;
+`;
+
 export const InputLable = styled.Text`
-  font-family: 'Pretendard-Black';
+  font-family: 'Pretendard-Bold';
+  align-self: flex-start;
+  margin-left: 20px;
+  font-size: 14px;
+  margin-top: 15px;
+`;
+
+export const InputGroup = styled.View`
+  width: 100%;
+  margin-top: 15px;
+`;
+
+export const SignUpInfo = styled.Text`
+  font-family: 'Pretendard-Light';
+  margin-left: 20px;
+  align-self: flex-start;
+  margin-bottom: 50px;
 `;
