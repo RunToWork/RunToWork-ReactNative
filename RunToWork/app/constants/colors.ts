a547f477e51f01c962149d876145f2d72ad119fe
greeneeya <suzythetravelerr@gmail.com>
2022-12-19 23:58:31 +0900

    색상 상수화

---
+++ RunToWork/app/constants/colors.ts
@@ -0,0 +1,16 @@
+export enum Color {
+  BLUE = '#4e6bff',
+  WHITE = '#ffffff',
+  WHITETWO = '#e5e5e5',
+  BLACK = '#242424',
+  BLACKTWO = '#000',
+  GREYPURPLE = '#807f82',
+  PALEGREY = '#f4f4f9',
+  PALEGREYTWO = '#eeeef1',
+  COOLGREY = '#a8acbb',
+  PALELILAC = '#d8e0ff',
+  GREYISH_TWO = '#b2b2b2',
+  CERULEAN = '#0099cc',
+  RED = '#FF3B1F',
+  ORANGE = '#FF7F00',
+}
